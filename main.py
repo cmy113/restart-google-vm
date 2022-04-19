a69839f3038220e2c806fcd5e48530ba0ac8da57
cmy113 <chenming.yong13@gmail.com>
2022-04-19 22:08:42 +0800

    logging no longer supported will use print instead

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import googleapiclient.discovery
 import time
-import logging
-
+# Remember to set timeout to 540s or at least 120s
 # Define variables
-PROJECT = "my-blog-345711"
+PROJECT = "xyz"
 ZONE = "us-west1-b"
-INSTANCE = "test"
+INSTANCE = "ghost"
 
 def restart_vm(event, context):
     compute = googleapiclient.discovery.build('compute', 'v1')
 
     # Stop VM
-    logging.info(f"Stopping instance: {INSTANCE} in project: {PROJECT} in zone: {ZONE} ")
+    print(f"Stopping instance: {INSTANCE} in project: {PROJECT} in zone: {ZONE}")
+
     result = compute.instances().stop(project=PROJECT, zone=ZONE, instance=INSTANCE).execute()
 
     # Sleep for 90s for it to fully stop
-    logging.info("Sleep for 90s")
+    print("Sleep for 90s")
     time.sleep(90)
 
     # Start VM
-    logging.info(f"Starting instance: {INSTANCE} in project: {PROJECT} in zone: {ZONE} ")
+    print(f"Starting instance: {INSTANCE} in project: {PROJECT} in zone: {ZONE}")
     result = compute.instances().start(project=PROJECT, zone=ZONE, instance=INSTANCE).execute()
